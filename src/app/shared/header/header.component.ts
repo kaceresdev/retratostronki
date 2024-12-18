e486637f83f5a645243d7991e7f9672a0d4dd5f8
Javier Cáceres Bordallo <kaceresdev@gmail.com>
2024-12-18 23:22:10 +0100

    Restyling header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
-import { Router, RouterLink } from "@angular/router";
+import { NgClass } from "@angular/common";
+import { Component, HostListener, Input } from "@angular/core";
+import { NavigationEnd, Router, RouterLink } from "@angular/router";
+import { filter } from "rxjs";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
 })
@@ -35,8 +37,21 @@ export class HeaderComponent {
       route: "/private-sessions",
     },
   ];
+  cristalType: boolean = false;
+  isTop: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.router.events.pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      const currentUrl = event.urlAfterRedirects;
+      if (currentUrl === "/") {
+        this.cristalType = true;
+        this.isTop = true;
+      } else {
+        this.cristalType = false;
+        this.isTop = false;
+      }
+    });
+  }
 
   toggleNavIcon() {
     const nav = document.getElementById("nav-icon");
@@ -57,6 +72,20 @@ export class HeaderComponent {
       window.scrollTo({ top: 0, behavior: "smooth" });
     } else {
       window.scrollTo({ top: 0 });
+    }
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    if (this.cristalType) {
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+      // Define el valor en píxeles desde el que quieres que se aplique el cambio
+      if (scrollPosition > 0) {
+        this.isTop = false;
+      } else {
+        this.isTop = true;
+      }
     }
   }
 }
